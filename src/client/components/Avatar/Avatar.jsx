17d2049c2d6a842db12c8c0652d0db8d0b9251fb
Rabii Luena <itisrabii@gmail.com>
2020-12-22 15:16:43 +0300

    feat: adding avatar components

---
+++ src/client/components/Avatar/Avatar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Avatar = ({ size, className, src, alt }) => {
+  if (size === 'sm') {
+    className += ' h-6 w-6';
+  }
+
+  if (size === 'md') {
+    className += ' h-8 w-8';
+  }
+
+  if (size === 'lg') {
+    className += ' h-10 w-10';
+  }
+
+  return <img className={`rounded-full ${className}`} src={src} alt={alt} />;
+};
+
+Avatar.defaultProps = {
+  size: 'md',
+  className: '',
+};
+
+Avatar.propTypes = {
+  /** Size of an avatar. */
+  size: PropTypes.string,
+
+  /** Class names to be used. */
+  className: PropTypes.string,
+
+  /** Altenative text. */
+  alt: PropTypes.string.isRequired,
+
+  /** Source of the image */
+  src: PropTypes.string.isRequired,
+};
+
+export default Avatar;
--- src/client/components/index.js
+++ src/client/components/index.js
@@ -2,5 +2,6 @@ import LayoutManager from './LayoutManager';
 import Head from './Head';
 import Link from './Link';
 import Button from './Button';
+import Avatar from './Avatar';
 
-export { LayoutManager, Head, Link, Button };
+export { LayoutManager, Head, Link, Button, Avatar };
