c4edeaa50b0474c8c53b65ffa3c7b81aae061e13
Rabii Luena <itisrabii@gmail.com>
2020-12-22 17:38:36 +0300

    feat: adding Comment component

---
+++ src/client/components/Comment/Comment.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Avatar, Link } from '@app/components';
+
+const Comment = ({ data }) => (
+  <div className="flex max-w-sm">
+    <Avatar src={data.profile} alt={data.author} />
+    <section className="pl-3">
+      <header>
+        <p className="text-sm font-semibold text-neutral-700">{data.author}</p>
+        <p className="text-xs text-neutral-500">{data.createdAt}</p>
+      </header>
+      <section className="text-sm py-3">{data.description}</section>
+      <footer>
+        <Link href="/" className="text-sm">
+          Reply
+        </Link>
+      </footer>
+    </section>
+  </div>
+);
+
+Comment.propTypes = {
+  data: PropTypes.objectOf(PropTypes.shape).isRequired,
+};
+
+export default Comment;
--- src/client/components/index.js
+++ src/client/components/index.js
@@ -4,5 +4,6 @@ import Link from './Link';
 import Button from './Button';
 import Avatar from './Avatar';
 import PostCard from './PostCard';
+import Comment from './Comment';
 
-export { LayoutManager, Head, Link, Button, Avatar, PostCard };
+export { LayoutManager, Head, Link, Button, Avatar, PostCard, Comment };
