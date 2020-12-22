09f1b4453fdec45a3d4ea7e28ddf3a47c242f226
Rabii Luena <itisrabii@gmail.com>
2020-12-22 07:14:33 +0300

    feat: adding Link component

---
+++ src/client/components/Link/Link.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import LinkWrapper from 'next/link';
+
+const Link = ({ children, size, href, className, active, ...props }) => {
+  if (size === 'xs') {
+    className += ' text-xs';
+  }
+
+  if (size === 'sm') {
+    className += ' text-sm';
+  }
+
+  if (size === 'md') {
+    className += ' text-md';
+  }
+
+  if (size === 'lg') {
+    className += ' text-lg';
+  }
+
+  return (
+    <LinkWrapper prefetch={false} href={href} {...props}>
+      <a
+        className={`text-pink-600 hover:text-pink-800 transition duration-150 ease-in-out ${className}`}
+        href={href}
+      >
+        {children}
+      </a>
+    </LinkWrapper>
+  );
+};
+
+Link.defaultProps = {
+  active: false,
+  className: '',
+  size: 'md',
+};
+
+Link.propTypes = {
+  /** Children content. */
+  children: PropTypes.node.isRequired,
+
+  /** Relative or absolute URL. */
+  href: PropTypes.string.isRequired,
+
+  /** Extended class names. */
+  className: PropTypes.string,
+
+  /**  */
+  active: PropTypes.bool,
+  size: PropTypes.string,
+};
+
+export default Link;
