1c7fdd8985ed601ba0deb2a1827ab2db0b8032d7
Rabii Luena <itisrabii@gmail.com>
2020-12-22 20:14:52 +0300

    feat: improving comment icons

--- src/client/components/Comment/Comment.jsx
+++ src/client/components/Comment/Comment.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Avatar, Link } from '@app/components';
+import { HeartIcon, HeartOutlineIcon } from '@app/components/Icons';
 
 const Comment = ({ data }) => (
   <div className="flex max-w-sm p-2">
@@ -11,10 +12,16 @@ const Comment = ({ data }) => (
         <p className="text-xs text-neutral-500">{data.createdAt}</p>
       </header>
       <section className="text-sm py-3">{data.description}</section>
-      <footer>
-        <Link href="/" className="text-sm">
+      <footer className="flex">
+        <Link href="/" className="text-md font-semibold">
           Reply
         </Link>
+        <div className="flex ml-auto">
+          <button type="button">
+            <HeartIcon variant="primary" size="sm" />
+          </button>
+          <span className="text-sm italic text-neutral-600">45</span>
+        </div>
       </footer>
     </section>
   </div>
