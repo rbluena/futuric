5e5a56289f6bbb4a990d4df227c085e88f6d2ff7
Rabii Luena <itisrabii@gmail.com>
2020-12-26 20:53:46 +0300

    feat: adding container component for user's header

---
+++ src/client/containers/ProfileHeaderContainer/ProfileHeaderContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ProfileHeader } from '@app/components';
+
+const ProfileHeaderContainer = () => (
+  <ProfileHeader
+    profile={{
+      name: 'Netflix',
+      website: 'https://netflix.com',
+      description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    }}
+  />
+);
+
+export default ProfileHeaderContainer;
