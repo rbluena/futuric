300d97a49412140e245e110f22ceefa7ee4e06c1
Rabii Luena <itisrabii@gmail.com>
2020-12-26 20:52:50 +0300

    feat: updating header component for user's profile

---
+++ src/client/components/ProfileHeader/ProfileHeader.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, Avatar, Button } from '@app/components';
+import { BadgeShieldIcon, CheckUserIcon } from '@app/components/Icons';
+
+const ProfileHeader = ({ profile, isMyProfile }) => (
+  <div className="header border-b  border-neutral-200">
+    <div className="mr-auto">
+      <div className="flex flex-wrap-reverse items-center">
+        <Avatar initials="NL" size="xl" className="text-2xl" />
+
+        <div className="flex flex-col items-start pl-2">
+          <Link
+            href="/@netflix"
+            variant="secondary"
+            size="lg"
+            className="font-bold flex items-center"
+          >
+            <span>{profile.name}</span>
+            &nbsp;
+            {profile.prominent && (
+              <BadgeShieldIcon size="sm" className="text-primary-700" />
+            )}
+            {/* <BadgeIcon size="xs" className="text-success-700" /> */}
+          </Link>
+          {profile.website && (
+            <Link href="/@" size="xs" variant="secondary" className="mb-2">
+              {profile.website}
+            </Link>
+          )}
+        </div>
+
+        {!isMyProfile && (
+          <div className="ml-auto">
+            {profile.following ? (
+              <Button variant="primary" className="flex items-center">
+                Unfollow&nbsp;&nbsp;
+                <CheckUserIcon size="xs" />
+              </Button>
+            ) : (
+              <Button variant="primary" outline className="flex items-center">
+                Follow
+              </Button>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+
+    <div className=" pl-16 pb-4">
+      {profile.description && (
+        <p className="text-sm text-neutral-900">{profile.description}</p>
+      )}
+    </div>
+  </div>
+);
+
+ProfileHeader.defaultProps = {
+  isMyProfile: false,
+};
+
+ProfileHeader.propTypes = {
+  profile: PropTypes.objectOf(PropTypes.shape).isRequired,
+  isMyProfile: PropTypes.bool,
+};
+export default ProfileHeader;
