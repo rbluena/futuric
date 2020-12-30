9035b0791e786aa0c71fbc1cf888d564b69006be
Rabii Luena <itisrabii@gmail.com>
2020-12-30 08:27:24 +0300

    refactor: reactoring authentication modal

--- src/client/containers/AuthModalContainer/AuthModalContainer.jsx
+++ src/client/containers/AuthModalContainer/AuthModalContainer.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal } from '@app/components';
 import Register from './Register';
 
-const AuthModalContainer = () => (
-  <Modal isOpen>
-    <Modal.Header />
-    <Modal.Content>
-      <Register />
-    </Modal.Content>
-  </Modal>
-);
+const AuthModalContainer = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={closeModal}>
+      <Modal.Header onClose={closeModal} />
+      <Modal.Content>
+        <Register />
+      </Modal.Content>
+    </Modal>
+  );
+};
 
 AuthModalContainer.propTypes = {};
 
--- src/client/containers/AuthModalContainer/Register/Register.jsx
+++ src/client/containers/AuthModalContainer/Register/Register.jsx
@@ -5,11 +5,11 @@ import Footer from './Footer';
 import Header from './Header';
 
 const Register = () => (
-  <div className="max-w-xs max-h-screen mx-auto py-6 overflow-y-auto">
+  <div className="max-w-xs mx-auto py-2 overflow-y-auto">
     <Header />
     <RegisterForm />
     <p className="text-neutral-600 text-2xl text-center mb-4">or</p>
-    <SignupGoogle className="" />
+    <SignupGoogle />
     <Footer />
   </div>
 );
