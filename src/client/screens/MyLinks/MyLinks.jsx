b657aa6d824fcd3646fd5e17777d7d49ba034939
Rabii Luena <itisrabii@gmail.com>
2021-01-04 10:14:45 +0300

    feat: on user's created links page, show create link button if links was created

--- src/client/screens/MyLinks/MyLinks.jsx
+++ src/client/screens/MyLinks/MyLinks.jsx
@@ -1,18 +1,50 @@
 import React from 'react';
-import { Section, ContentWrapper } from '@app/components';
+import router from 'next/router';
+import { Button, Section, ContentWrapper } from '@app/components';
+import { useSelector, useDispatch } from 'react-redux';
+import { myLinksSelector } from '@app/selectors';
+import { getMyLinksAction } from '@app/actions';
 import ProfileHeaderContainer from '@app/containers/ProfileHeaderContainer';
-import MyLinksContainer from '@app/containers/MyLinksContainer';
+import PostsContainer from '@app/containers/PostsContainer';
+import ViewMoreButton from './ViewMoreButton';
 
-const MyLinks = () => (
-  <div className="pb-4">
-    <ContentWrapper>
-      <ProfileHeaderContainer />
-      <Section heading="Created Links">
-        <MyLinksContainer />
-      </Section>
-    </ContentWrapper>
-  </div>
-);
+const MyLinks = () => {
+  const { data, meta } = useSelector(myLinksSelector);
+  const dispatch = useDispatch();
+  const { hasNextPage, nextPage, limit } = meta;
+
+  function loadMore() {
+    dispatch(getMyLinksAction({ page: nextPage, limit }));
+  }
+
+  return (
+    <div className="pb-4">
+      <ContentWrapper>
+        <ProfileHeaderContainer />
+        <Section heading="My Links">
+          {data && data.length > 0 ? (
+            <PostsContainer posts={data} />
+          ) : (
+            <div className="flex flex-col items-center py-6">
+              <h2 className="text-xl mb-6">
+                You haven&apos;t created any link.
+              </h2>
+              <Button
+                variant="primary"
+                onClick={() => router.push('/links/create')}
+                size="lg"
+                className="max-w-xs"
+              >
+                Create link
+              </Button>
+            </div>
+          )}
+        </Section>
+        {hasNextPage && <ViewMoreButton onClick={() => loadMore()} />}
+      </ContentWrapper>
+    </div>
+  );
+};
 
 MyLinks.propTypes = {};
 
