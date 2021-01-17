ae9809698bf275f2c7d5da8349304f5a99593f51
Rabii Luena <itisrabii@gmail.com>
2021-01-17 10:58:05 +0300

    refactor: refactoring footer

--- src/client/components/Footer/Footer.jsx
+++ src/client/components/Footer/Footer.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
-import { Link } from '@app/components';
+import { Link, DotSeparator } from '@app/components';
 
 const Footer = () => (
-  // <div className="transform -translate-x-1/2 left-1/2 w-full p-4 bg-primary-100 absolute bottom-0 flex max-w-6xl"></div>
+  // <div className="transform -translate-x-2/2 left-1/2 w-full p-4 bg-primary-100 absolute bottom-0 flex max-w-6xl"></div>
   <div className="p-4 bg-primary-100 fixed bottom-0 flex max-w-6xl mx-auto">
-    <div className="text-neutral-600">
-      {new Date().getFullYear()}&nbsp;&copy;&nbsp;Asteyo
+    <div className="text-neutral-900 font-bold">
+      {new Date().getFullYear()}&nbsp;&copy;&nbsp;Asteyo, Inc.
     </div>
-    <span>&nbsp;|&nbsp;&nbsp;&nbsp;</span>
-    <ul className="flex">
-      <li className="pr-2">
+    <span className="pl-2">|</span>
+    <ul className="flex font-bold items-center">
+      {/* <li className="pr-2">
         <Link href="/about-us" size="sm">
           About
         </Link>
-      </li>
-      <li className="pr-2">
+      </li> */}
+      <li className="px-2">
         <Link href="/privacy" size="sm">
           Privacy
         </Link>
       </li>
-      <li className="pr-2">
+      <DotSeparator variant="primary" />
+      <li className="px-2">
         <Link href="/terms" size="sm">
           Terms of Use
         </Link>
       </li>
-      <li className="pr-2">
+      <DotSeparator variant="primary" />
+      <li className="px-2">
         <Link href="/learn" size="sm">
           Learn
         </Link>
