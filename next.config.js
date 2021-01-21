38f16948b3405c26374e4106f991a64c709382e1
Rabii Luena <itisrabii@gmail.com>
2021-01-21 07:13:09 +0300

    fix: fixing cache issue related to build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   distDir: '_next',
-  generateBuildId: async () => {
-    const id = process.env.BUILD_ID;
-    return id;
-  },
   env: {
     SERVER_API: process.env.SERVER_API,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
