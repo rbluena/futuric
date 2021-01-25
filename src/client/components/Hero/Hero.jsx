6abd31a0bb6d0ca578612513a689bd5f6a0f5b9b
Rabii Luena <itisrabii@gmail.com>
2021-01-25 20:54:48 +0300

    feat: changing hero title

--- src/client/components/Hero/Hero.jsx
+++ src/client/components/Hero/Hero.jsx
@@ -12,12 +12,11 @@ const Hero = () => {
   return (
     <div className="text-center px-2 py-24 bg-primary-800 text-neutral-50 ">
       <h1 className="text-3xl md:text-4xl mb-8 font-bold">
-        {/* Discover what&apos;s coming next on the internet. */}
-        Create link for your future online content.
+        Discover what&apos;s coming next on the internet.
       </h1>
       <h2 className="text-2xl font-light">
-        {/* Publisher can claim and share a URL ahead of time. <br /> */}
-        Get discovered early on by the community.
+        Create link for your future online content. <br />
+        Get notified when content is available online.
       </h2>
 
       {!isAuthenticated && (
