27fdee413d4a365374a28771b177af00d077252e
Rabii Luena <itisrabii@gmail.com>
2021-01-26 17:16:53 +0300

    refactor: trying to make sense on Hero

--- src/client/components/Hero/Hero.jsx
+++ src/client/components/Hero/Hero.jsx
@@ -12,11 +12,14 @@ const Hero = () => {
   return (
     <div className="text-center px-2 py-24 bg-primary-800 text-neutral-50 ">
       <h1 className="text-3xl md:text-4xl mb-8 font-bold">
-        Discover what&apos;s coming next on the internet.
+        Create link for your future online content.
       </h1>
-      <h2 className="text-2xl font-light">
-        Create link for your future online content. <br />
-        Get notified when content is available online.
+      <h2 className="text-2xl">
+        {/* Create and share link for the content will be published online in the
+        near future.
+        <br /> */}
+        Discover contents will soon be available online, <br />
+        and get notified when they become available.
       </h2>
 
       {!isAuthenticated && (
